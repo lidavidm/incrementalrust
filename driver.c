73ef4b8aaf8efc93ca390eda58fc3ec91fc22678
David Li <li.davidm96@gmail.com>
2016-05-30 09:12:49 -0400

    feat: add fixnum, nil, bool support

--- driver.c
+++ driver.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+
+#define FIXNUM_MASK 3
+#define FIXNUM_TAG  0
+#define BOOL_MASK 0b1111111
+#define BOOL_TAG 0b0011111
+
+#define IS_FIXNUM(x) ((x & FIXNUM_MASK) == FIXNUM_TAG)
+#define CONVERT_FIXNUM(x) ((x >> 2))
+
+#define IS_EMPTY_LIST(x) ((x == 0b00101111))
+
+#define IS_BOOL(x) ((x & BOOL_MASK) == BOOL_TAG)
+#define CONVERT_BOOL(x) ((x >> 7))
+
 int main(int argc, char **argv) {
-  printf("%d\n", scheme_entry());
+  int val = scheme_entry();
+  if (IS_FIXNUM(val)) {
+    printf("%d\n", CONVERT_FIXNUM(val));
+  }
+  else if (IS_EMPTY_LIST(val)) {
+    printf("()\n");
+  }
+  else if (IS_BOOL(val)) {
+    if (CONVERT_BOOL(val)) {
+      printf("#t\n");
+    }
+    else {
+      printf("#f\n");
+    }
+  }
   return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -27,27 +27,112 @@ impl Amd64Backend {
         }
     }
 
-    fn compile(&mut self, sexp: &Sexp) -> &str {
-        if let Sexp::Atom(Atom::I(num)) = *sexp {
-            emit!(self, "movl ${}, %eax", num);
-            emit!(self, "ret");
+    fn immediate_rep(sexp: &Sexp) -> u32 {
+        match *sexp {
+            Sexp::Atom(ref atom) => match *atom {
+                Atom::I(integer) => (integer << 2) as u32,
+                Atom::F(_) => panic!("Unimplemented: representation of float"),
+                Atom::S(_) => panic!("Unimplemented: representation of string"),
+                Atom::B(true) => 0b10011111,
+                Atom::B(false) => 0b00011111,
+            },
+            Sexp::List(ref items) => if items.is_empty() {
+                0b00101111
+            }
+            else {
+                panic!("Unimplemented: representation of non-empty list");
+            }
         }
+    }
+
+    fn compile(&mut self, sexp: &Sexp) -> &str {
+        emit!(self, "movl ${}, %eax", Self::immediate_rep(sexp));
+        emit!(self, "ret");
 
         &self.buffer
     }
 }
 
-fn compile_program(sexp: &Sexp) -> String {
+pub fn compile_program(sexp: &Sexp) -> String {
     let mut backend = Amd64Backend::new();
     backend.compile(sexp).to_owned()
 }
 
 fn main() {
-    print!("{}", compile_program(&parse("42").unwrap()));
+    use std::io::{self, BufRead};
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+    for line in handle.lines() {
+        let line = line.unwrap();
+        if let Ok(result) = parse(&line) {
+            println!("Parsed: {:?}", result);
+            println!("{}", compile_program(&result));
+        }
+        else {
+            println!("{:?}", line);
+        }
+    }
 }
 
-#[test]
-fn it_works() {
-    let source = compile_program(&parse("42").unwrap());
-    print!("{}", source);
+#[cfg(test)]
+mod test {
+    extern crate tempfile;
+    use sexp::parse;
+    use super::*;
+
+    use std::fs::{self, File};
+    use std::io::Write;
+    use std::path::Path;
+    use std::process::Command;
+
+    fn assemble(asm_path: &Path) -> String {
+        let obj_file = tempfile::NamedTempFile::new().expect("Could not create temp object file");
+        let obj_path = obj_file.path();
+        let cmd = Command::new("as")
+            .arg("-o")
+            .arg(obj_path)
+            .arg(asm_path)
+            .status().expect("Could not assemble");
+        assert!(cmd.success());
+
+        let bin_file = tempfile::NamedTempFile::new().expect("Could not create temp binary file");
+        let bin_path = bin_file.path();
+        let cmd = Command::new("gcc")
+            .arg("-o")
+            .arg(bin_path)
+            .arg(obj_path)
+            .arg("driver.c")
+            .status().expect("Could not compile");
+        assert!(cmd.success());
+
+        let actual_bin_path = bin_path.with_file_name("incrementalrust_temp");
+        fs::copy(bin_path, &actual_bin_path);
+
+        String::from_utf8_lossy(&Command::new(actual_bin_path)
+            .output().expect("Could not execute").stdout).into_owned()
+    }
+
+    fn compile_and_execute(input: &str) -> String {
+        let source = compile_program(&parse(input).unwrap());
+        let mut f = tempfile::NamedTempFile::new().expect("Could not create temp file");
+        f.write_all(&source.into_bytes()).expect("Could not write generated asm to test file");
+        f.sync_all().expect("Could not sync test file");
+        assemble(f.path())
+    }
+
+    #[test]
+    fn fixnum() {
+        assert_eq!(compile_and_execute("42"), "42\n");
+    }
+
+    #[test]
+    fn empty_list() {
+        assert_eq!(compile_and_execute("()"), "()\n");
+    }
+
+    #[test]
+    fn boolean() {
+        assert_eq!(compile_and_execute("#t"), "#t\n");
+        assert_eq!(compile_and_execute("#f"), "#f\n");
+    }
 }
