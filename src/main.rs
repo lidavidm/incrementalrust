2602756e83405734ff37d189a69efccc729847d4
David Li <li.davidm96@gmail.com>
2016-06-10 22:54:26 -0400

    feat: add binary +

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 extern crate sexp;
+extern crate tempfile;
+
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
 
 use sexp::{Atom, Sexp, parse};
 
+#[derive(Debug)]
+enum CompileError {
+    // Procedure, expected, actual
+    NumArgs(String, usize, usize),
+}
+
+type Result<T> = std::result::Result<T, CompileError>;
+
 struct Amd64Backend {
     buffer: String,
+    stack_location: usize,
 }
 
 macro_rules! emit {
@@ -24,6 +39,7 @@ impl Amd64Backend {
         buffer.push_str("scheme_entry:\n");
         Amd64Backend {
             buffer: buffer,
+            stack_location: 1,
         }
     }
 
@@ -44,6 +60,21 @@ impl Amd64Backend {
         }
     }
 
+    fn push(&mut self) -> isize {
+        let location = self.peek();
+        emit!(self, "movl %eax, {}(%rsp)", location);
+        self.stack_location += 1;
+        location
+    }
+
+    fn peek(&self) -> isize {
+        (-4) * (self.stack_location as isize)
+    }
+
+    fn pop(&mut self) {
+
+    }
+
     // Emits the representation for true or false, based on the result
     // of a prior comparison.
     fn emit_boolean(&mut self) {
@@ -52,64 +83,78 @@ impl Amd64Backend {
         emit!(self, "orl $31, %eax");
     }
 
-    fn compile_primitive(&mut self, op: &str, tail: &[Sexp]) -> bool {
-        if tail.len() != 1 {
-            panic!("add1 has multiple args");
-        }
-
-        self.compile(&tail[0]);
+    fn compile_unary_primitive(&mut self, op: &str, tail: &[Sexp]) -> Result<bool> {
+        // TODO: need to check length, but only if it's a valid op
         match op {
             "add1" => {
+                self.compile(&tail[0]);
                 emit!(self, "addl ${}, %eax", Self::immediate_rep(&Atom::I(1)));
-                true
             }
 
             "char->integer" => {
+                self.compile(&tail[0]);
                 // TODO: check the tag first
                 emit!(self, "shr $6, %eax");
-                true
             }
 
             "integer->char" => {
+                self.compile(&tail[0]);
                 emit!(self, "shl $6, %eax");
                 emit!(self, "orl $15, %eax");
-                true
             }
 
             "null?" => {
+                self.compile(&tail[0]);
                 emit!(self, "andl $255, %eax");
                 emit!(self, "cmpl $47, %eax");
                 self.emit_boolean();
-                true
             }
 
             "zero?" => {
+                self.compile(&tail[0]);
                 emit!(self, "cmpl $0, %eax");
                 self.emit_boolean();
-                true
             }
 
             "integer?" => {
+                self.compile(&tail[0]);
                 emit!(self, "andl $3, %eax");
                 emit!(self, "cmpl $0, %eax");
                 self.emit_boolean();
-                true
             }
 
             "boolean?" => {
+                self.compile(&tail[0]);
                 emit!(self, "andl $127, %eax");
                 emit!(self, "cmpl $31, %eax");
                 self.emit_boolean();
-                true
             }
 
             "not" => {
+                self.compile(&tail[0]);
                 emit!(self, "xorl $128, %eax");
-                true
             }
 
-            _ => false
+            _ => return Ok(false)
+        };
+
+        Ok(true)
+    }
+
+    fn compile_binary_primitive(&mut self, op: &str, tail: &[Sexp]) -> Result<bool> {
+        // TODO: need to check len, but only if it's a valid op
+        match op {
+            "+" => {
+                self.compile(&tail[1]);
+                let location = self.push();
+                self.compile(&tail[0]);
+                emit!(self, "addl {}(%rsp), %eax", location);
+            }
+
+            _ => return Ok(false)
         }
+
+        Ok(true)
     }
 
     fn compile(&mut self, sexp: &Sexp) -> &str {
@@ -123,8 +168,17 @@ impl Amd64Backend {
             else {
                 let (head, tail) = items.split_at(1);
                 if let Sexp::Atom(Atom::S(ref arg)) = head[0] {
-                    if !self.compile_primitive(arg, tail) {
-                        panic!("Invalid primitive: {}", arg);
+                    // TODO: make this less messy
+                    match self.compile_unary_primitive(arg, tail) {
+                        Ok(true) => (),
+                        Ok(false) => {
+                            match self.compile_binary_primitive(arg, tail) {
+                                Ok(true) => (),
+                                Ok(false) => panic!("Unrecognized primitive: {}", arg),
+                                Err(err) => panic!("{:?}", err),
+                            }
+                        },
+                        Err(err) => panic!("{:?}", err),
                     }
                 }
             }
@@ -146,6 +200,48 @@ pub fn compile_program(sexp: &Sexp) -> String {
     backend.compile_program(sexp).to_owned()
 }
 
+
+fn assemble(asm_path: &Path) -> String {
+    let obj_file = tempfile::NamedTempFileOptions::new()
+        .prefix("incrementalrust")
+        .suffix(".o")
+        .create().expect("Could not create temp object file");
+    let obj_path = obj_file.path();
+    let cmd = Command::new("as")
+        .arg("-o")
+        .arg(obj_path)
+        .arg(asm_path)
+        .status().expect("Could not assemble");
+    assert!(cmd.success());
+
+    let bin_file = tempfile::NamedTempFileOptions::new()
+        .prefix("incrementalrust")
+        .suffix(".bin")
+        .create().expect("Could not create temp binary file");
+    let bin_path = bin_file.path();
+    let cmd = Command::new("gcc")
+        .arg("-o")
+        .arg(bin_path)
+        .arg(obj_path)
+        .arg("driver.c")
+        .status().expect("Could not compile");
+    assert!(cmd.success());
+
+    let actual_bin_path = bin_path.with_extension("exec");
+    fs::copy(bin_path, &actual_bin_path).expect("Could not copy file!");
+
+    String::from_utf8_lossy(&Command::new(actual_bin_path)
+                            .output().expect("Could not execute").stdout).into_owned()
+}
+
+pub fn compile_and_execute(input: &str) -> String {
+    let source = compile_program(&parse(input).unwrap());
+    let mut f = tempfile::NamedTempFile::new().expect("Could not create temp file");
+    f.write_all(&source.into_bytes()).expect("Could not write generated asm to test file");
+    f.sync_all().expect("Could not sync test file");
+    assemble(f.path())
+}
+
 fn main() {
     use std::io::{self, BufRead};
     let stdin = io::stdin();
@@ -155,6 +251,8 @@ fn main() {
         if let Ok(result) = parse(&line) {
             println!("Parsed: {:?}", result);
             println!("{}", compile_program(&result));
+            println!("");
+            println!("{}", compile_and_execute(&line));
         }
         else {
             println!("{:?}", line);
@@ -164,55 +262,7 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    extern crate tempfile;
-    use sexp::parse;
     use super::*;
-
-    use std::fs;
-    use std::io::Write;
-    use std::path::Path;
-    use std::process::Command;
-
-    fn assemble(asm_path: &Path) -> String {
-        let obj_file = tempfile::NamedTempFileOptions::new()
-            .prefix("incrementalrust")
-            .suffix(".o")
-            .create().expect("Could not create temp object file");
-        let obj_path = obj_file.path();
-        let cmd = Command::new("as")
-            .arg("-o")
-            .arg(obj_path)
-            .arg(asm_path)
-            .status().expect("Could not assemble");
-        assert!(cmd.success());
-
-        let bin_file = tempfile::NamedTempFileOptions::new()
-            .prefix("incrementalrust")
-            .suffix(".bin")
-            .create().expect("Could not create temp binary file");
-        let bin_path = bin_file.path();
-        let cmd = Command::new("gcc")
-            .arg("-o")
-            .arg(bin_path)
-            .arg(obj_path)
-            .arg("driver.c")
-            .status().expect("Could not compile");
-        assert!(cmd.success());
-
-        let actual_bin_path = bin_path.with_extension("exec");
-        fs::copy(bin_path, &actual_bin_path).expect("Could not copy file!");
-
-        String::from_utf8_lossy(&Command::new(actual_bin_path)
-            .output().expect("Could not execute").stdout).into_owned()
-    }
-
-    fn compile_and_execute(input: &str) -> String {
-        let source = compile_program(&parse(input).unwrap());
-        let mut f = tempfile::NamedTempFile::new().expect("Could not create temp file");
-        f.write_all(&source.into_bytes()).expect("Could not write generated asm to test file");
-        f.sync_all().expect("Could not sync test file");
-        assemble(f.path())
-    }
 
     #[test]
     fn fixnum() {
@@ -287,5 +337,14 @@ mod test {
     fn not() {
         assert_eq!(compile_and_execute("(not #t)"), "#f");
         assert_eq!(compile_and_execute("(not #f)"), "#t");
+    }
+
+    #[test]
+    fn binary_add() {
+        assert_eq!(compile_and_execute("(+ 0 0)"), "0");
+        assert_eq!(compile_and_execute("(+ 0 2)"), "2");
+        assert_eq!(compile_and_execute("(+ 2 0)"), "2");
+        assert_eq!(compile_and_execute("(+ 1 3)"), "4");
+        assert_eq!(compile_and_execute("(+ (char->integer #\\a) 3)"), "100");
     }
 }
